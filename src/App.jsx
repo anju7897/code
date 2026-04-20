0b54f208343018166eceec4bdb884a804c466ce8
Anju Prajapati <anjuprajapati7318@gmail.com>
2026-04-20 13:10:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import Home from "./Home";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
+import Holydays from "./component/Holydays";
+
 function App() {
+
+  function showMessage() {
+    alert("Button Clicked!");
+  }
+
   return (
     <div>
-      <h1>Hello React 🚀</h1>
+
 
       <Home />
       <Contact />
       <Holydays />
       <Footer />
-      
     </div>
   );
 }
