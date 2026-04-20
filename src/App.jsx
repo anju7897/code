75d1d92d2bd2ca01595e560024b30fab04eddcc9
Anju Prajapati <anjuprajapati7318@gmail.com>
2026-04-20 13:10:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 function App() {
-
-  function showMessage() {
-    alert("Button Clicked!");
-  }
-
   return (
     <div>
       <h1>Hello React 🚀</h1>
 
-      <p>This is a simple React app</p>
-
-      <button onClick={showMessage}>
-        Click Me
-      </button>
+      <Home />
+      <Contact />
+      <Holydays />
+      <Footer />
+      
     </div>
   );
 }
